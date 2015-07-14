cf68f6ebb58b5b60a0fea170d2894ff0e8a4602c
Josh Richardson <joshrichardson1234@hotmail.com>
2015-07-14 11:07:43 +1000

    Dynamic blog page advanced

--- wp-content/themes/joshwa/home.php
+++ wp-content/themes/joshwa/home.php
@@ -9,8 +9,33 @@
 
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <h1><?php the_title(); ?></h1>
-            <p><?php the_content(); ?></p>
+
+
+
+            <article class="post">
+              <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+              <h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
+              <ul class="post-meta no-bullet">
+                <li class="author">
+                  <a href="author.html">
+                    <span class="wpt-avatar small">
+                      <?php echo get_avatar( get_the_author_meta( 'ID' ), 24 ); ?>
+                    </span>
+                    by <?php the_author_posts_link(); ?>
+                  </a>
+                </li>
+                <li class="cat">in <?php the_category(); ?></li>
+                <li class="date">in <?php the_date(); ?></li>
+              </ul>
+              <?php if ( get_the_post_thumbnail() ) : ?>
+              <div class="img-container">
+                <?php the_post_thumbnail('large'); ?>
+              </div>
+            <?php endif; ?>
+            </article>
+
+
+
 
           <?php endwhile; else : ?>
       		<p><?php _e( 'Sorry, no pages found.' ); ?></p>
